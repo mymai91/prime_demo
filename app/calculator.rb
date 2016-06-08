2cc9fb921ce1fb6f49776f32bcd1513d549a8faa
Jany <my.maithi@gmail.com>
2016-06-08 22:17:06 +0700

    Solve problem 1:  Find the sum of all the primes below 2 million

---
+++ app/calculator.rb
@@ -0,0 +1,22 @@
+require_relative '../app/prime'
+
+MAX_NUMBER = 2_000_000
+
+# Calculator
+# Solve problem 1
+class Calculator
+  include Prime
+
+  # Print result sum_of_primes
+  # param default is MAX_NUMBER
+  # return @number
+  def sum_of_primes(number = MAX_NUMBER)
+    sum_list_primes(number)
+  end
+end
+
+cal = Caculator.new
+
+result = cal.sum_of_primes
+
+puts "The sum of the primes below #{MAX_NUMBER} is: #{result}"
